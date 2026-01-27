4d07c094a9c8b4279a65c1b40b8397bf96c836a6
Denilson87 <denilsonmucavel@outlook.com>
2026-01-27 23:23:25 +0200

    adding xsd requests and response config

---
+++ src/main/java/spring_loan_soap/SpringLoanSoapApplication.java
@@ -0,0 +1,13 @@
+package spring_loan_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoanSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoanSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_loan_soap/SpringLoanSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_loan_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoanSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
